a019680440ae844ae057adc2962e00bec1886fb7
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-24 17:02:16 -0400

    Arithmetic Operators

---
+++ R/1_allArithmeticOperatorsInR.r
@@ -0,0 +1,10 @@
+# R program to illustrate the use of Arithmetic operators
+vec1 <- c(0, 2)
+vec2 <- c(2, 3)
+# Performing operations on Operands
+cat ("Addition of vectors :", vec1 + vec2, "\n")
+cat ("Subtraction of vectors :", vec1 - vec2, "\n")
+cat ("Multiplication of vectors :", vec1 * vec2, "\n")
+cat ("Division of vectors :", vec1 / vec2, "\n")
+cat ("Modulo of vectors :", vec1 %% vec2, "\n")
+cat ("Power operator :", vec1 ^ vec2,"\n")
