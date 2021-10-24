ebc435e09eb004512a8473a32cb76163678a0d3a
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-24 17:02:46 -0400

    LogicalOperators

---
+++ R/2_allLogicalOperatorsInR.r
@@ -0,0 +1,9 @@
+# R program to illustrate the use of Logical operators
+vec1 <- c(0,2)
+vec2 <- c(TRUE,FALSE)
+# Performing operations on Operands
+cat ("Element wise AND :", vec1 & vec2, "\n")
+cat ("Element wise OR :", vec1 | vec2, "\n")
+cat ("Logical AND :", vec1 && vec2, "\n")
+cat ("Logical OR :", vec1 || vec2, "\n")
+cat ("Negation :", !vec1, "\n")
