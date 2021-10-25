b6439afafb586ff92e0facd9c74a2860d56f8cdf
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-25 16:34:21 -0400

    Rules for naming the variables

---
+++ R/9_rulesForNamingTheVariables.r
@@ -0,0 +1,16 @@
+# R program to demonstrate  rules for naming the variables 
+  
+# Correct naming
+b2 = 7 
+  
+# Correct naming
+Amiya_Profession = "Student" 
+  
+# Correct naming
+Amiya.Profession = "Student" 
+  
+# Wrong naming
+2b = 7 
+  
+# Wrong naming
+Amiya@Profession = "Student" 
