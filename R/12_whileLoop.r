db5691a1f7248a79bc270b95666ca588b44f22a4
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-25 17:05:47 -0400

    Create 12_whileLoop.r

---
+++ R/12_whileLoop.r
@@ -0,0 +1,22 @@
+# While Loop
+# It is a type of control statement which will run a statement or a set 
+# of statements repeatedly unless the given condition becomes 
+# false.
+# Syntax:
+# while ( condition )
+#  { 
+#      statement 
+# }
+
+
+# R program to demonstrate the use of while loop
+   
+val = 1
+   
+# using while loop
+while (val <= 5 ) 
+{
+    # statements
+    print(val)
+    val = val + 1
+}
