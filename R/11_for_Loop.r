649e766b34d3fc71fe069b78598b5e6f65836271
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-25 17:05:43 -0400

    Create 11_for_Loop.r

---
+++ R/11_for_Loop.r
@@ -0,0 +1,16 @@
+# For Loop
+# Syntax
+# for   (value in sequence)
+#  { 
+#         Statement
+#  }
+
+
+#  R program to demonstrate the use of for loop 
+   
+# using for loop
+for (val in 1:5)
+{
+    # statement 
+    print(val)
+}
