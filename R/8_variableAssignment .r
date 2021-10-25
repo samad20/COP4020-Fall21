2dade973df8bd85de9777c9f9c1936d703bd4783
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-25 16:34:02 -0400

    dDemonstrate  variable assignment

---
+++ R/8_variableAssignment .r
@@ -0,0 +1,19 @@
+# R program to demonstrate  variable assignment  
+  
+# Assignment using equal operator
+var1 = c(0, 1, 2, 3)
+print(var1)
+  
+# Assignment using leftward operator
+var2 <- c("Python", "R")
+print(var2)
+  
+# A Vector Assignment
+a = c(1, 2, 3, 4)
+print(a)
+b = c("Debi", "Sandeep", "Subham", "Shiba")
+print(b)
+  
+# A group of vectors Assignment using list
+c = list(a, b)
+print(c)
