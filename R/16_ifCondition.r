423c0b56a3953a25faedc2dbe14bce287258696b
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-10-25 17:06:00 -0400

    Create 16_ifCondition.r

---
+++ R/16_ifCondition.r
@@ -0,0 +1,17 @@
+# if condition
+# • This control structure checks the expression provided in 
+# parenthesis is true or not. If true, the execution of the 
+# statements in braces {} continues.
+# Syntax:
+# if (expression){ 
+#             statements 
+#   ....
+#  ....
+#  }
+
+x <- 100
+  
+if (x > 10)
+{
+    print(paste(x, "is greater than 10"))
+}
